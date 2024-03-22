cc6b07a86489c4bad3c9074e63b886f6e111a114
Letícia Gabrielle <130669115+leticia44@users.noreply.github.com>
2024-03-22 11:02:46 -0300

    first commit

---
+++ vite-project/src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import "../Styles/Footer.css"
+
+function Footer() {
+    return(
+        <footer>
+            <p className="copyrigth">2024 - Escola SENAI</p>
+        </footer>
+    )
+}
+
+export default Footer
